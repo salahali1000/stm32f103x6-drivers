0d501ccb6a84d164e3311620e26059a792065b3a
salahali1000 <124151783+salahali1000@users.noreply.github.com>
2023-09-24 02:32:01 +0300

    Create Std_Types.h

---
+++ inc/Services/Std_Types.h
@@ -0,0 +1,27 @@
+/*
+ * STD_TYPES.h
+ *
+ *  Created on: Aug 21, 2023
+ *      Author: salah
+ */
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+typedef signed char                   sint8_t;
+typedef unsigned char                 uint8_t;
+typedef signed short                  sint16_t;
+typedef unsigned short                uint16_t;
+typedef unsigned long                 uint32_t;
+typedef signed long                   sint32_t;
+typedef signed long long              sint64_t;
+typedef unsigned long long     	      uint64_t;
+typedef volatile unsigned char         vuint8;
+typedef volatile unsigned short        vunit16;
+typedef volatile unsigned long         vuint32;
+typedef volatile unsigned long long    vuint64;
+typedef volatile signed char           vsint8;
+typedef volatile signed short          vsint16;
+typedef volatile signed long           vsint32;
+typedef volatile signed long long      vsint64;
+
+#endif /* STD_TYPES_H_ */
